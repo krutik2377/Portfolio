981fb24327d2b5b987ffa433c383eed824dbddcd
krutik2377 <krutikgevariya7723@gmail.com>
2023-09-27 22:49:12 -0400

    Projects Section Modified

--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -27,16 +27,10 @@ const Projects = ({openModal,setOpenModal}) => {
             <ToggleButton value="web app" onClick={() => setToggle('web app')}>WEB APP'S</ToggleButton>
           }
           <Divider />
-          {toggle === 'android app' ?
-            <ToggleButton active value="android app" onClick={() => setToggle('android app')}>ANDROID APP'S</ToggleButton>
+          {toggle === 'Artificial Intelligence' ?
+            <ToggleButton active value="Artificial Intelligence" onClick={() => setToggle('Artificial Intelligence')}>ARTIFICIAL INTELLIGENCE</ToggleButton>
             :
-            <ToggleButton value="android app" onClick={() => setToggle('android app')}>ANDROID APP'S</ToggleButton>
-          }
-          <Divider />
-          {toggle === 'machine learning' ?
-            <ToggleButton active value="machine learning" onClick={() => setToggle('machine learning')}>MACHINE LEARNING</ToggleButton>
-            :
-            <ToggleButton value="machine learning" onClick={() => setToggle('machine learning')}>MACHINE LEARNING</ToggleButton>
+            <ToggleButton value="Artificial Intelligence" onClick={() => setToggle('Artificial Intelligence')}>ARTIFICIAL INTELLIGENCE</ToggleButton>
           }
         </ToggleButtonGroup>
         <CardContainer>
